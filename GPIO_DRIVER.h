ba38d8603c91ba4f094f4e5778e010fe0487cbf8
osalem192 <124590406+osalem192@users.noreply.github.com>
2023-02-10 15:46:11 +0200

    Update GPIO_DRIVER.h

--- GPIO_DRIVER.h
+++ GPIO_DRIVER.h
@@ -12,7 +12,8 @@ void Set_Bin_Dir(uint8 port, uint8 pinnum, uint8 dir);
 void Pin_Write(uint8 port, uint8 pinnum, uint8 dir);
 void Pin_Toggle(uint8 port, uint8 pinnum);
 uint8 Pin_Read(uint8 port, uint8 pinnum);
-// you can change the whole port by simply changing pinnum to 0xFF
+void Set_Port_Dir(uint8 port, uint8 dir);
+void Port_Write(uint8 port, uint8 dir);
 
 
 #endif /* GPIO_DRIVER_H_ */
